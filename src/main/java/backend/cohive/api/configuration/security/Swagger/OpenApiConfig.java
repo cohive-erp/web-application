7a1bc63886dc77c9d677e9e981269190cb806bc6
Gustavo Sena <gustavo.sena@atos.net>
2024-05-08 15:02:01 -0300

    Mudando o nome do responsável da documentação

--- src/main/java/backend/cohive/api/configuration/security/Swagger/OpenApiConfig.java
+++ src/main/java/backend/cohive/api/configuration/security/Swagger/OpenApiConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
                 title = "Projeto Usuários",
                 description = "Implementação de JWT com Spring Security",
                 contact = @Contact(
-                        name = "Diego",
+                        name = "Gustavo",
                         url = "https://github.com/cohive-erp/web-application",
                         email = "gustavo.sena@sptech.school"
                 ),
